7c437cf4729c6e4cf9678237678b9b525fcccf83
Hani Abdelrahman <hanyouf37@gmail.com>
2024-10-20 21:49:14 +0300

    Session01

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,18 +1,34 @@
 
+using Microsoft.EntityFrameworkCore;
+using Store.Data.Context;
+using Store.Repository.Interfaces;
+using Store.Repository.UnitOfWork;
+using Store.Web.Helper;
+
 namespace Store.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static  async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+           
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaltConnection"));
+
+            }
+            );
+
 
             var app = builder.Build();
 
@@ -22,10 +38,12 @@ namespace Store.Web
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            await ApplySeeding.ApplySeedingAsync(app);
 
 
             app.MapControllers();
